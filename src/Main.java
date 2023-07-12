de1d64faa24caf04a24173885f3182eeb0e30854
thelonelinux <vickythetechguy@gmail.com>
2023-07-13 01:52:36 +0530

    LongestCommonPrefix - Leetcode (easy question)

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,8 @@ import java.util.Map;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    public static  String longestCommonPrefix(String[] strs) {
-
-        //Here we have done code for LongestCommonSubstring and it is different from longest common prefix. so check it.
-
-       return "";
-    }
     public static void main(String[] args) {
-        String[] strs= {"dog","racecar","car"};
-        System.out.println(longestCommonPrefix(strs));
+
 
     }
 }
