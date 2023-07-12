45d2e20e8928977e6bf0897263177b37ddff8eff
thelonelinux <vickythetechguy@gmail.com>
2023-07-13 01:56:46 +0530

    OverAllLearningAndTipsForBetterSolutionAndApproach

---
+++ src/OverAllLearningAndTipsForBetterSolutionAndApproach.java
@@ -0,0 +1,9 @@
+public class OverAllLearningAndTipsForBetterSolutionAndApproach {
+/**
+
+    1. Use of stringBuilder is more space saving and good for appending rather than string concatenation.
+        Concatenation is more time consuming in string then appending in stringBuilder.
+
+ */
+
+}
