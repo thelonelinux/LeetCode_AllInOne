e3489a2673e0d45caed094a137b8a8e845f34068
thelonelinux <vickythetechguy@gmail.com>
2023-07-16 01:26:37 +0530

    RemoveDuplicates (Leetcode - Easy Problem)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ import java.util.*;
 
 public class Main {
 
+
     public static void main(String[] args) {
+
+        int[] arr={0,1,1,2,2,3,4,4};
+
+
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
