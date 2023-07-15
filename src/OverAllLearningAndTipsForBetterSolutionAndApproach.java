48d6bb7b6c57c51c4cea28ee3b665849bf0a1f30
thelonelinux <vickythetechguy@gmail.com>
2023-07-16 01:49:57 +0530

    OverAllLearningAndTipsForBetterSolutionAndApproach (About Notes Dissolving - Why)

--- src/OverAllLearningAndTipsForBetterSolutionAndApproach.java
+++ src/OverAllLearningAndTipsForBetterSolutionAndApproach.java
@@ -1,8 +1,25 @@
+/**
+ * Never in your life now make notes for competitive programming solutions,
+ * whatever you already have is fine but not now
+ *
+ * Best way to learn competitive programming is learning question, understanding it, writing on a paper and trying to solve in that way first
+ * looking for discussions, looking for constraints and taking care of edge cases accordingly. so all this stuffs you can never find in
+ * notes of code and questions, specially in your leetcode notes also.
+ *
+ * So if you have anything to add in any problem. Now onwards we will be adding here in this repo only related to problems, solutions,
+ * anything new learning and DSA learning all can be kept here and can be accessed from everywhere
+ *
+ * You can also add those pdfs here, or share the link from drive stuff things.
+ *
+ * Probably slowly we are going to remove written notes and put all here. Either in code form with better and progressing expalanation
+ * or those best pages in form of PDF or CamScanner.
+ */
+
 public class OverAllLearningAndTipsForBetterSolutionAndApproach {
 /**
 
-    1. Use of stringBuilder is more space saving and good for appending rather than string concatenation.
-        Concatenation is more time consuming in string then appending in stringBuilder.
+    1. Use of stringBuilder is more space-saving and good for appending rather than string concatenation.
+        Concatenation is more time-consuming in string than appending in stringBuilder.
 
  */
 
