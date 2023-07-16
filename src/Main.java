85b48ad62e3a5c8c2f386184af5a23d24ddc618f
thelonelinux <vickythetechguy@gmail.com>
2023-07-17 01:39:01 +0530

    BestTimeToBuyAndSellStock (LeetCode - Easy Problem)

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] arr={0,1,1,2,2,3,4,4};
-
-
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
-
     }
 }
 
