174ddbbf820a7e17c315a2b8d8e12a8a0770fd69
thelonelinux <vickythetechguy@gmail.com>
2023-07-17 17:40:43 +0530

    ValidAnagram (Leetcode - Easy Problem)

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,8 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        int[] arr={2,4,1};
 
-        //System.out.println(containsDuplicate(arr));
-
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-
-        }
     }
 }
 
